ebd1a1ee86634697eed59e5b12939a811d6ced75
cheesy008 <mr.world008@gmail.com>
2020-07-29 14:56:00 +0600

    d

--- egov.js
+++ egov.js
@@ -2,19 +2,24 @@ const puppeteer = require("puppeteer");
 const fetch = require("node-fetch");
 const fs = require("fs");
 const FormData = require("form-data");
+var xmlenc = require('xml-encryption');
+var SignedXml = require('xml-crypto').SignedXml	 
 
 const LOGIN_URL = 'https://idp.egov.kz/idp/sign-in';
 const PDF_URL = 'https://egov.kz/services/P30.01/#/declaration/0/,/';
 const CAPTCHA_SOLVER_URL = 'http://0.0.0.0:4000/egov-captcha-solver/';
+const SEND_EDS_URL = 'https://egov.kz/services/P30.01/rest/app/send-eds?captchaCode='
 
 
 const egov = {
     browser: null,
     page: null,
+    captcha: null,
 
     initialize: async () => {
         this.browser = await puppeteer.launch({
-            headless: false
+            headless: false,
+            defaultViewport: null
         });
 
         this.page = await this.browser.newPage();
@@ -66,7 +71,7 @@ const egov = {
             .then(data => data.message)
     },
 
-    getPdf: async (egovBin) => {
+    passCaptcha: async (egovBin) => {
         await this.page.goto(PDF_URL, {
             waitUntil: 'networkidle2',
             timeout: 0
@@ -77,16 +82,77 @@ const egov = {
         await this.page.waitFor(3000);
 
         const captchaEl = await this.page.$('img[id="captcha_picture"]');
-
         await egov.downloadCaptcha(captchaEl);
 
-        const captcha = await egov.decryptCaptcha();
-
-        await this.page.type('input[id="input_captcha"]', captcha);
+        this.captcha = await egov.decryptCaptcha();
+        await this.page.type('input[id="input_captcha"]', this.captcha);
 
         await this.page.$eval('button[id="searchSignButton"]', el => el.click());
 
+        // this.page.waitForNavigation({ waitUntil: 'networkidle2' }); 
     },
+
+    sendCertificate: async () => {
+        const btnSelector = "#sign > div > div > div > div:nth-child(2) > eds > div > div > div:nth-child(2) > figure > figcaption > a";
+        await this.page.waitForSelector(btnSelector);
+        await this.page.evaluate(({
+            btnSelector
+        }) => {
+            const btn = document.querySelector(btnSelector);
+            btn.click();
+        }, {
+            btnSelector
+        })
+
+        await this.page.waitFor(5000);
+
+        const url = SEND_EDS_URL + this.captcha;
+        // const url = "https://egov.kz/services/P30.01/rest/app/xml";
+
+
+        // body = {
+        //     declarantUin: "140140015980",
+        //     bin: "140140015980"
+        // }
+        // const options = {
+        //     rsa_pub: fs.readFileSync('./certificate/cert.pub'),
+        //     pem: fs.readFileSync('./certificate/cert.pem'),
+        //     encryptionAlgorithm: 'http://www.w3.org/2001/04/xmlenc#aes256-cbc',
+        //     keyEncryptionAlgorithm: 'http://www.w3.org/2001/04/xmlenc#rsa-oaep-mgf1p',
+        //     disallowEncryptionWithInsecureAlgorithm: true,
+        //     warnInsecureAlgorithm: true
+        // };
+
+        // const signedXml = xmlenc.encrypt('blabla', options, (err, result) => {
+        //     console.log(err, result);
+        // })
+        var xml = "<library>" +
+	            "<book>" +
+	              "<name>Harry Potter</name>" +
+	            "</book>" +
+              "</library>"
+              
+        var sig = new SignedXml()
+        sig.addReference("//*[local-name(.)='book']")    
+        sig.signingKey = fs.readFileSync("./certificate/GOSTKNCA.p12")
+        sig.computeSignature(xml)
+        fs.writeFileSync("./certificate/signed.xml", sig.getSignedXml())
+
+
+        const data = fs.ReadStream('./certificate/signed.xml');
+
+        await fetch(url, {
+                'method': 'POST',
+                body: data,
+            }).then(resp => resp.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        console.log('test');
+    }
 
     // getPdf: async () => {
     //     await this.page.goto(pdfUrl, {waitUntil: 'networkidle2'});
--- index.js
+++ index.js
@@ -10,80 +10,8 @@ const egovPassword = process.env.EGOV_PASSWORD;
 
     await egov.login(egovBin, egovPassword);
 
-    await egov.getPdf(egovBin);
+    await egov.passCaptcha(egovBin);
+
+    await egov.sendCertificate();
 
 })();
-
-// const puppeteer = require("puppeteer");
-// const fs = require("fs");
-
-
-// (async () => {
-//     browser = await puppeteer.launch({
-//         headless: false
-//     });
-
-//     page = await browser.newPage();
-
-//     await page.goto('https://idp.egov.kz/idp/sign-in', {waitUntil: 'networkidle2'});
-
-//     await page.evaluate(({
-//         egovBin,
-//         egovPassword
-//     }) => {
-//         const binInput = document.querySelector('input[class="field__input a-field__input inputiin"]');
-//         binInput.value = egovBin;
-
-//         const passwordInput = document.querySelector('input[class="field__input a-field__input inputpass"]');
-//         passwordInput.value = egovPassword;
-
-//         const button = document.querySelector('input[class="btn btn-primary btn-fit mt-3"]');
-//         button.click();
-
-//     }, {
-//         egovBin,
-//         egovPassword
-//     });
-
-//     await page.waitForNavigation();
-
-//     // const viewSource = await page.goto('https://intoli.com/blog/saving-images/img/logo.svg');
-
-
-//     fs.writeFile("./anu.jpg", await viewSource.buffer(), function (err) {
-//         if (err) {
-//             return console.log(err);
-//         }
-
-
-//         console.log("The file was saved!");
-//     })
-
-// })();
-
-// const fetch = require("node-fetch");
-// const FormData = require("form-data");
-// const fs = require("fs");
-
-// const capthaSolverUrl = 'http://0.0.0.0:4000/egov-captcha-solver/';
-
-
-// const file = fs.ReadStream('/home/cheesy008/javascript_devs/egov/captcha_images/captcha.jpeg');
-
-// const formData = new FormData();
-// formData.append('file', file)
-
-// const upload = async (file) => {
-//     return await fetch('http://0.0.0.0:4000/egov-captcha-solver/', { 
-//       method: 'POST',
-//       body: file 
-//     }).then(response => response.json())
-//       .then(data => data.message)
-//   };
-
-// //   upload(formData)
-
-// (async function() {
-//     const a = await upload(formData);
-//     console.log(a);
-//   })();
